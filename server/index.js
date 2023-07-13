ce331dc393fdf395a72b422628e36ffbd49928a1
Vladislav Kuznetsov <kuznets0v.vladislav@yandex.ru>
2023-07-13 21:21:56 +0300

    upd backend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const { PORT, MONGOURL } = process.env;
 const app = express();
+const authRouter = require('./routes/auth-router.js');
+const errorMiddleware = require('./middlewares/error-middleware.js');
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use('/api/auth', authRouter);
+app.use(errorMiddleware);
 
 (async () => {
     try {
