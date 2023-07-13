903776831e6afaa96f666a1dfef07e62f306c5ea
Vladislav Kuznetsov <kuznets0v.vladislav@yandex.ru>
2023-07-13 15:14:56 +0300

    simple app start

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log('Hello world!!!');
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const { PORT, MONGOURL } = process.env;
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(MONGOURL, { dbName: 'chat' });
+
+        app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+    }
+    catch (e) {
+
+    }
+})();
