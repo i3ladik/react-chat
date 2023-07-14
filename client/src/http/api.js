4f8b9a233c3e75e5d28ac33c87c5292f1d86564c
Vladislav Kuznetsov <kuznets0v.vladislav@yandex.ru>
2023-07-15 02:16:32 +0300

    added api

---
+++ client/src/http/api.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+const $api = axios.create({
+    withCredentials: true,
+    baseURL: process.env.REACT_APP_API_URL + '/api'
+});
+
+$api.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+    return config;
+});
+
+$api.interceptors.response.use((config) => {
+    return config;
+}, async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && error.config && !error.config._isRetry) {
+        originalRequest._isRetry = true;
+        try {
+            const response = await axios.get(`${API_URL}/api/auth/refresh`, { withCredentials: true });
+            localStorage.setItem('token', response.data.accessToken);
+            return $api.request(originalRequest);
+        } catch (e) {
+            console.error('Not authorized!')
+        }
+    }
+    throw error;
+});
+
+export default $api;
