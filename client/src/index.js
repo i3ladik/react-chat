16ac609f428d1bd43db5d94810fc154de1454858
Vladislav Kuznetsov <kuznets0v.vladislav@yandex.ru>
2023-07-15 02:14:58 +0300

    upd

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+
+import App from './components/App';
+import { store } from './reducers';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
