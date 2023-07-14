bdef299ed4b4cb671cd45357bc334ec1affc9c7c
Vladislav Kuznetsov <kuznets0v.vladislav@yandex.ru>
2023-07-15 02:13:38 +0300

    change expiresIn in jwt

--- server/services/token-service.js
+++ server/services/token-service.js
@@ -4,8 +4,8 @@ const tokenModel = require('../models/token-model.js');
 
 class TokenService {
     generateTokens(payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' });
-        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d' });
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '1m' });
+        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '1d' });
         return { refreshToken, accessToken };
     }
 
