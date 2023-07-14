80239c10d820b189f722616b2c1400dbfe8f347d
Vladislav Kuznetsov <kuznets0v.vladislav@yandex.ru>
2023-07-15 02:14:35 +0300

    move

---
+++ client/src/components/App.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import './app.css';
+import Navbar from './navbar/Navbar.jsx';
+import Registration from './auth/Registration.jsx';
+import Login from './auth/Login.jsx';
+import { auth } from "../actions/auth.js";
+
+function App() {
+    const isAuth = useSelector((state) => state.user.isAuth);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(auth());
+    }, [dispatch]);
+
+    return (
+        <BrowserRouter>
+            <div className='app'>
+                <Navbar />
+                <div className='wrap'>
+                    {!isAuth &&
+                        <Routes>
+                            <Route path='/registration' element={<Registration />} />
+                            <Route path='/login' element={<Login />} />
+                        </Routes>
+                    }
+                </div>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
