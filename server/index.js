9263c57e416ba154024746523914c405ffba0959
Vladislav Kuznetsov <kuznets0v.vladislav@yandex.ru>
2023-07-15 02:12:59 +0300

    upd cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ const authRouter = require('./routes/auth-router.js');
 const errorMiddleware = require('./middlewares/error-middleware.js');
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
 app.use(cookieParser());
 app.use('/api/auth', authRouter);
 app.use(errorMiddleware);
