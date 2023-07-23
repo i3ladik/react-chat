0bc78e7081cdc1e88182a130dda9550f48840832
Vladislav Kuznetsov <kuznets0v.vladislav@yandex.ru>
2023-07-23 22:40:49 +0300

    added message router

---
+++ server/routes/message-router.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const { body } = require('express-validator');
+
+const messageController = require('../controllers/message-controller.js');
+const authMiddleware = require('../middlewares/auth-middleware.js');
+const router = new Router();
+
+router.post('/message',
+    authMiddleware,
+    body('message').isLength({ min: 1, max: 200 }),
+    messageController.message
+);
+router.get('/messages', messageController.getMessages);
+
+module.exports = router;
