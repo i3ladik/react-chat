e353e13163e487b7bdd60b63f1bbe5215b83078c
Vladislav Kuznetsov <kuznets0v.vladislav@yandex.ru>
2023-07-23 22:40:37 +0300

    added websocket message router

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ require('dotenv').config();
 const { PORT, MONGOURL } = process.env;
 const app = express();
 const authRouter = require('./routes/auth-router.js');
+const messageRouter = require('./routes/message-router.js');
 const errorMiddleware = require('./middlewares/error-middleware.js');
+const { wsHandleUpgrade } = require('./routes/ws-router.js');
 
 app.use(express.json());
 app.use(cors({
@@ -18,7 +20,14 @@ app.use(cors({
 app.use(cookieParser());
 
 //server
+app.use('/api', messageRouter);
 app.use('/api/auth', authRouter);
+
+//websocket
+app.get('/api/ws', (req, res) => {
+    wsHandleUpgrade(req, req.socket, Buffer.alloc(0));
+});
+
 //client
 if (process.env.NODE_ENV !== 'development') {
     process.env.CLIENT_URL = process.env.API_URL;
@@ -34,7 +43,12 @@ app.use(errorMiddleware);
     try {
         await mongoose.connect(MONGOURL, { dbName: 'chat' });
 
-        app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+        const server = app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+
+        server.on('upgrade', (req, socket, head) => {
+            if (req.url === '/api/ws') wsHandleUpgrade(req, socket, head);
+            else socket.destroy();
+        });
     }
     catch (e) {
 
