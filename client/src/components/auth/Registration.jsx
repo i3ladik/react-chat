9bf39d05a423238aa7f54d7aa0b83ed7e57e6d0c
Vladislav Kuznetsov <kuznets0v.vladislav@yandex.ru>
2023-07-23 22:42:32 +0300

    registration rework

--- client/src/components/auth/Registration.jsx
+++ client/src/components/auth/Registration.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './registration.css';
 import Input from '../../utils/input/Input.jsx';
@@ -7,12 +8,24 @@ import { registration } from '../../actions/auth.js';
 const Registration = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [nickname, setNickname] = useState('');
+    const [isButDisbld, setisButDisbld] = useState(false);
+    const navigate = useNavigate();
+
+    const registrationHandle = async () => {
+        setisButDisbld(true);
+        const result = await registration(email, password, nickname);
+        setisButDisbld(false);
+        if(result) navigate('/');
+    };
+
     return (
         <div className='registration'>
             <div className='registration_header'>Registration</div>
+            <Input value={nickname} setValue={setNickname} type='text' placeholder='Nickname' />
             <Input value={email} setValue={setEmail} type='text' placeholder='Email' />
             <Input value={password} setValue={setPassword} type='password' placeholder='Password' />
-            <button className='registration_btn' onClick={() => registration(email, password)}>Sign Up</button>
+            <button className='registration_btn' onClick={registrationHandle} disabled={isButDisbld}>Sign Up</button>
         </div>
     );
 };
