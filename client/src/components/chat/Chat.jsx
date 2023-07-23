6ad3741d2aa77c35fd04cbcf0a30b3184531a3ec
Vladislav Kuznetsov <kuznets0v.vladislav@yandex.ru>
2023-07-23 22:43:08 +0300

    upd for messages

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -2,20 +2,34 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import './chat.css';
+import { getMessages, sendMessage } from '../../actions/message.js';
 import Li from '../../utils/li/Li.jsx';
 import Input from '../../utils/input/Input.jsx';
 
 const Chat = () => {
     const isAuth = useSelector((state) => state.user.isAuth);
-    const [text, setText] = useState('');
+    const [content, setContent] = useState('');
+    const [messageList, setMessageList] = useState([]);
 
-    let messages = [];
-    while (messages.length < 20) messages.push({ name: 'Tester', text: 'test' });
+    const gettingMessages = async () => {
+        const messages = await getMessages();
+        setMessageList((prevMessageList) => [...prevMessageList, ...messages]);
+    }
+
+    const sendHandle = async () => {
+        const result = await sendMessage(content);
+        if(result) setContent('');
+    }
+
     useEffect(() => {
-        const list = document.getElementsByClassName('chat_window_messages_list');
-        list.item(0).scrollTop = list.item(0).offsetHeight;
-        console.log(list.item(0).offsetHeight);
-
+        gettingMessages();
+        const wsocket = new WebSocket(`wss://${process.env.REACT_APP_API_URL}/api/ws`);
+        wsocket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            setMessageList((prevMessageList) => [...prevMessageList, data]);
+        };
+        const list = document.getElementsByClassName('chat_window_messages_list').item(0);
+        list.scrollTop = list.offsetHeight;
     }, []);
 
     return (
@@ -24,16 +38,16 @@ const Chat = () => {
                 <div className='chat_window'>
                     <div className='chat_window_messages'>
                         <ul className='chat_window_messages_list'>
-                            {messages.map((message, index) => (
-                                <Li key={index} name={message.name} text={message.text} />
+                            {messageList.map((message, index) => (
+                                <Li key={index} name={message.nickname} text={message.content} />
                             ))}
                         </ul>
                     </div>
                     {
                         isAuth &&
                         <div className='chat_window_typing'>
-                            <Input value={text} setValue={setText} type='text' placeholder='Enter the text here' />
-                            <button className='chat_window_typing_button'>Send</button>
+                            <Input value={content} setValue={setContent} type='text' placeholder='Enter the text here' />
+                            <button className='chat_window_typing_button' onClick={sendHandle}>Send</button>
                         </div>
                     }
                 </div>
